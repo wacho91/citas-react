fbd5cbed1d16880f2f59a59100c979690111bd4b
wacho91 <criskol.71@gmail.com>
2022-07-29 11:48:44 -0500

    Creacion de botones de eliminar y actualizar

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -17,6 +17,21 @@ const Paciente = ({paciente}) => {
           <p className='font-bold mb-3 text-gray-700 uppercase'>Sintomas: {''}
               <span className='font-normal normal-case'>{paciente.sintomas}</span>
           </p>
+          <div className="flex justify-between mt-10">
+            <button
+                type="button"
+                className="py-2 px-10 bg-indigo-600 
+                hover:bg-indigo-700 text-white font-bold
+                uppercase rounded-lg cursor-pointer">Editar
+            </button>
+
+
+            <button type="button"
+                className="py-2 px-10 bg-red-600 
+                hover:bg-red-700 text-white font-bold
+                uppercase rounded-lg cursor-pointer">Eliminar
+            </button>
+          </div>
     </div>
   )
 }
